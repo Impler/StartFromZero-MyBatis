37774e42307d2fad1ab207b179bcf1d704ab3e09
Impler <704090860@qq.com>
2016-04-19 15:50:05 +0800

    mybatis foreach

--- MyBatis/src/main/java/com/study/mybatis/dao/UserDao.java
+++ MyBatis/src/main/java/com/study/mybatis/dao/UserDao.java
@@ -8,17 +8,24 @@ import org.apache.ibatis.annotations.Param;
 import com.study.mybatis.domain.User;
 
 public interface UserDao extends BaseDao<User, Integer>{
-	//only for study test case below
+	//only for study dynamic sql test case below
+	//test choose when otherwise
 	public List<User> selectByCondition1(User entity);
+	//test where
 	public List<User> selectByCondition2(User entity);
+	//test trim
 	public List<User> selectByCondition3(User entity);
 	
+	//test set s
 	public int update1(User entity);
+	//test set e
 	
+	//test foreach s
 	public List<User> selectByIds(Integer[] ids);
 	public List<User> selectByIds1(List<Integer> ids);
 	
 	public List<User> selectByRoleAndStatus(List<Integer> roleIds, List<Integer> statusIds);
 	public List<User> selectByRoleAndStatus1(@Param("roleIds")List<Integer> roleIds, @Param("statusIds")List<Integer> statusIds);
 	public List<User> selectByRoleAndStatus1(Map<String, List<Integer>> roleAndStatus);
+	//test foreach e
 }
